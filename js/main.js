4e6ede4738bf85922a41ec33e7085932aba08f8d
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-05-25 17:05:50 -0300

    Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -28,6 +28,37 @@
 // console.log(fruta.nome);
 // alert(fruta.cor);
 
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
-console.log(frutas);
-alert(frutas[1].nome);
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var idade = prompt("Qual sua idade? ");
+// if(idade >= 18){
+//     alert("maior de idade");
+// }else{
+//     alert("menor de idade");
+// };
+
+//================ WHILE
+// var count = 0;
+// while(count< 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// }
+//================
+
+//================
+// var count;
+// for(count = 0; count <= 5; count++){
+//     alert(count);
+// }
+//================
+
+// var d = new Date();
+// alert(d.getMonth()+1);
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+
+
