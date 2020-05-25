f9ad865c539f4af4554ddb0ac0a3a6a8e45002e4
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-05-25 16:31:04 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var nome = "Wesley";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo!";
+
+// alert(nome + " tem " + idade + " anos.");
+
+// alert(idade + idade2);
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
